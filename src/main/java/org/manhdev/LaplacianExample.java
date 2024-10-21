037c0d3038c1463824f4044d0e7309c244f91c9d
manhz2003 <manhthenguyen2003@gmail.com>
2024-10-21 17:57:01 +0700

    create bt 2

---
+++ src/main/java/org/manhdev/LaplacianExample.java
@@ -0,0 +1,29 @@
+package org.manhdev;
+
+import boofcv.alg.filter.derivative.GImageDerivativeOps;
+import boofcv.io.image.ConvertBufferedImage;
+import boofcv.struct.border.BorderType;
+import boofcv.struct.image.GrayF32;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class LaplacianExample {
+    public static void main(String[] args) throws IOException {
+        // Đọc ảnh
+        BufferedImage input = ImageIO.read(new File("/Users/nguyenthemanh/Downloads/anhdemo.jpg"));
+
+        // Chuyển đổi sang ảnh grayscale
+        GrayF32 gray = ConvertBufferedImage.convertFromSingle(input, null, GrayF32.class);
+
+        // Áp dụng Laplacian Gaussian với BorderType
+        GrayF32 laplacian = new GrayF32(gray.width, gray.height);
+        GImageDerivativeOps.laplace(gray, laplacian, BorderType.EXTENDED);  // Thêm BorderType.EXTENDED
+
+        // Lưu ảnh kết quả
+        BufferedImage output = ConvertBufferedImage.convertTo(laplacian, null);
+        ImageIO.write(output, "png", new File("laplacian_output.png"));
+    }
+}
---
+++ src/main/java/org/manhdev/SobelExample.java
@@ -0,0 +1,37 @@
+package org.manhdev;
+
+import boofcv.abst.filter.derivative.ImageGradient;
+import boofcv.factory.filter.derivative.FactoryDerivative;
+import boofcv.io.image.ConvertBufferedImage;
+import boofcv.struct.image.GrayF32;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class SobelExample {
+    public static void main(String[] args) throws IOException {
+        // Đọc ảnh
+        BufferedImage input = ImageIO.read(new File("/Users/nguyenthemanh/Downloads/anhdemo.jpg"));
+
+        // Chuyển đổi ảnh sang định dạng GrayF32
+        GrayF32 gray = ConvertBufferedImage.convertFromSingle(input, null, GrayF32.class);
+
+        // Khởi tạo ảnh gradient cho x và y
+        GrayF32 gradientX = new GrayF32(gray.width, gray.height);
+        GrayF32 gradientY = new GrayF32(gray.width, gray.height);
+
+        // Áp dụng toán tử Sobel
+        ImageGradient<GrayF32, GrayF32> sobel = FactoryDerivative.sobel(GrayF32.class, null);
+        sobel.process(gray, gradientX, gradientY);
+
+        // Hiển thị gradient kết quả (gradientX và gradientY chứa thông tin gradient theo hai chiều)
+        BufferedImage outputX = ConvertBufferedImage.convertTo(gradientX, null);
+        BufferedImage outputY = ConvertBufferedImage.convertTo(gradientY, null);
+
+        // Lưu ảnh kết quả
+        ImageIO.write(outputX, "png", new File("sobel_gradient_x.png"));
+        ImageIO.write(outputY, "png", new File("sobel_gradient_y.png"));
+    }
+}
